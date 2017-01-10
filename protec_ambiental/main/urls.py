ddeeeaf7bf9d8903a5a9bb9db534db3a68c0ed60
Carolina <Carolina.k.salinas@gmail.com>
2017-01-10 13:40:13 -0500

    Add templates of law and oshas

--- protec_ambiental/main/urls.py
+++ protec_ambiental/main/urls.py
@@ -3,4 +3,7 @@ from django.utils.translation import ugettext as _
 
 urlpatterns = patterns('main.views',
                url(r'^$', 'home', name='home'),
+               url(r'^oshas/$', 'oshas', name='oshas'),   
+               url(r'^law/$', 'law', name='law'),   
+
            )
--- protec_ambiental/main/views.py
+++ protec_ambiental/main/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from .models import Company
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def home(request):
 	return render(request,'main/home.html')
+
+
+#panel de oshas
+def oshas(request):
+	return render(request,'main/oshas.html')
+
+
+#panel de ley de seguridad ambiental
+def law(request):
+	#if user is admin 
+	companies = Company.objects.all()
+	#else 
+		#companies = Company.objects.get(su empresa)
+	return render(request, "main/ley_seguridad.html", locals())
