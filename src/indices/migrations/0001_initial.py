3dd7cb288ec2bd6608ffdd4fd19495820341305b
bicho <bichocj@gmail.com>
2017-08-31 18:29:59 -0500

    update migrations

--- src/indices/migrations/0001_initial.py
+++ src/indices/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0002_auto_20170831_1016'),
+        ('main', '0001_initial'),
     ]
 
     operations = [
